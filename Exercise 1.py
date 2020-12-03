5e4416346354244dc196e8cbbb9df8beabf3145b
Jamie Rowe <jpkrowe@hotmail.co.uk>
2020-12-03 12:30:52 +0000

    The program can now work out the eigenvalues and their degeneracies for the linear and cylic polyenes.

--- Exercise 1.py
+++ Exercise 1.py
@@ -1,30 +1,46 @@
 import numpy as np
 
+
 def eigenvalues(matrix):
     evals, evecs = np.linalg.eig(matrix)
     return evals
 
-class Matrix: ###creates a matrix class in which the
+
+def degeneracies(evals): ### creates a matrix where the ijth element is 1 if i and j are degenerate eigenavalues
+    degeneracy = np.zeros((len(evals), len(evals)))
+    for i in range(len(evals)):
+        for j in range(len(evals)):
+            if abs(evals[i]-evals[j]) < 0.00001:
+                degeneracy[i][j] = 1
+            else:
+                degeneracy[i][j] = 0
+    return degeneracy
+
+
+class Matrix: ###Creates a matrix class in which the matrix built is dependent on the option and length chosen
     def __init__(self, length, option):
         self.matrix = np.zeros((length, length))
-        if option == 1:
-            a = -np.ones(length - 1)
-            np.fill_diagonal(self.matrix[1:], a)
-            np.fill_diagonal(self.matrix[:, 1:], a)
-        elif option == 2:
-            for i in range(length):
-                if 0 < i < length - 1:
-                    self.matrix[i, i + 1] = -1
-                    self.matrix[i, i - 1] = - 1
-                elif i > 0:
-                    self.matrix[i, i - 1] = - 1
-                else:
-                    self.matrix[i, i + 1] = - 1
+        a = -np.ones(length - 1)
+        np.fill_diagonal(self.matrix[1:], a)
+        np.fill_diagonal(self.matrix[:, 1:], a)
+        if option == 2:
+            self.matrix[length - 1][0] = - 1
+            self.matrix[0][length - 1] = - 1
         self.evals = eigenvalues(self.matrix)
 
 
-length = int(input("State length of linear polyene"))
-matrix  = Matrix(length, 1)
+length = int(input("State length of polyene:"))
+option = int(input("Choose an option: (1 for linear, 2 for cyclic)"))
+matrix = Matrix(length, option)
 evals = matrix.evals
-print(evals)
-print(matrix.matrix)
+evals.sort()
+degeneracy = degeneracies(evals)
+checked = np.zeros(len(evals))
+for i in range(len(evals)):
+    if checked[i] == 0:
+        degen = 0
+        for j in range(len(evals)):
+            degen = degen + degeneracy[i][j]
+            if degeneracy[i][j] == 1:
+                checked[j] = 1
+        print("Eigenvalue = " + str(evals[i]) + " Degeneracy = " + str(degen))
